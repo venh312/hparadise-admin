58595fb00a6ff951b3688e44b03c6b4a5ffde8c6
venh <conf312@naver.com>
2023-07-30 13:57:18 +0900

    [+] 공통 URL 설정 적용

--- src/main/resources/static/js/url-strings.js
+++ src/main/resources/static/js/url-strings.js
@@ -3,7 +3,9 @@ const url = {
   member: {
     signin: '/member/public/signin',
     signup: '/member/public/signup',
-    forgot-password: '/member/public/forgot-password',
+    forgot: {
+      password: '/member/public/forgot-password'
+    }
     list: '/member/list',
     detail: '/member/detail'
   }
