651bccda72e61e6982c54b6a3631ebc16d9ea26f
conf312 <conf312@naver.com>
2023-08-02 19:42:25 +0900

    Feature/work window (#21)
    * [↔] Headers 동적 처리, 로그인 예외처리
    
    * [+] SUCCESS 추가
    
    * [+] SUCCESS 추가
    
    * [+] toStringDateTime 추가
    
    * [+] 대시보드 추가
    
    * [+] fetch 공통 모듈 적용
    
    * [+] 성공(600) 코드 추가
    
    * [+] CSRF 예외 페이지 적용
    
    * [+] 샘플 에러 안내 페이지
    
    * [+] 관리자 목록 페이지 적용
    
    * [+] 샘플 대시보드 페이지
    
    * [+] 관리자 목록 URL 추가
    
    * [+] fetch 데이터 결과 반환 추가
    
    * [+] 타임리프 공통 레이아웃 설정
    
    * [+] 공통 URL 설정 적용
    
    * [+] 공통 URL 설정 적용
    
    * [+] 공통 URL 설정 적용
    
    * [+] 로딩바 적용
    
    * [+] 로그인 성공시 처리를 사용자 정의하여 사용함으로 수동으로 로그인 성공시 XSRF-TOKEN 쿠키 설정
    
    * [+] PUT, DELETE fetch 추가
    
    * [+] 공통 loading bar 추가
    
    * [+] 사용자 상세 추가
    
    * [+] develop 동기화
    
    * [+] 이메일 중복 체크, 테이블 크기 고정
    
    * [+] LNB 현재 메뉴 활성화 추가
    
    * [+] 서브 타이틀 수정

--- src/main/resources/static/js/ui-functions.js
+++ src/main/resources/static/js/ui-functions.js
@@ -1,3 +1,16 @@
 const loadingDisplay = function(flag) {
   document.getElementById('loading').style.display = flag;
 }
+
+document.addEventListener('DOMContentLoaded', function() {
+  // [lnb] Current page add class active
+  const lnbMenu = document.querySelectorAll('.menu-item');
+  lnbMenu.forEach(element => {
+    if (element.className.indexOf(location.pathname) > -1) {
+      element.classList.add('active')
+    } else {
+      element.classList.remove('active');
+    }
+  });
+});
+
