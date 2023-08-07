608a846a4caa628fc78b81ad6868a9c1ede8936d
venh <conf312@naver.com>
2023-08-07 20:57:24 +0900

    [+] 공통 함수로 이동, 날짜 변경 이벤트 함수 추가

--- src/main/resources/static/js/ui-functions.js
+++ src/main/resources/static/js/ui-functions.js
@@ -2,6 +2,57 @@ const loadingDisplay = function(flag) {
   document.getElementById('loading').style.display = flag;
 }
 
+const changeDate = function(obj) {
+  const startDate = document.getElementById('startDate');
+  const endDate = document.getElementById('endDate');
+  if (startDate.value && endDate.value) {
+    if (obj.id == 'startDate' && startDate.value.replace(/-/g, '') > endDate.value.replace(/-/g, ''))
+      endDate.value = startDate.value;
+    else (startDate.value.replace(/-/g, '') > endDate.value.replace(/-/g, ''))
+      startDate.value = endDate.value;
+  }
+}
+
+const fnPageMove = function(obj) {
+  const pageItem = document.querySelectorAll('.page-item');
+  pageItem.forEach(function(element, index) {
+    element.classList.remove('active');
+  });
+  obj.parentNode.classList.add('active');
+  document.getElementById('page').value = obj.getAttribute('data-page');
+  document.getElementById('pageSize').value = obj.getAttribute('data-pageSize');
+  fnSearch();
+}
+
+const fnPageItemHtml = function(resultMap) {
+  let html = '<li class="page-item first" id="pageItemFirst">';
+  html += '<button class="page-link" data-page="0" data-pageSize="8" onclick="fnPageMove(this)">';
+  html += '<i class="tf-icon bx bx-chevrons-left"></i>';
+  html += '</button>';
+  html += '</li>';
+
+  for (var i = resultMap.startPage; i <= resultMap.endPage; i++) {
+    let page = resultMap.pageSize * (i - 1);
+    if (resultMap.currentPage == i)
+      html += '<li class="page-item active">';
+    else
+      html += '<li class="page-item">';
+    html += '<button class="page-link" id="pageLink" data-page="'+page+'" data-pageSize="'+resultMap.pageSize+'" onclick="fnPageMove(this)">'+i+'</button>';
+    html += '</li>';
+  }
+
+  const lastPage = (resultMap.totalCnt - resultMap.pageSize);
+  if (lastPage >= resultMap.pageSize) {
+    html += '<li class="page-item last">';
+    html += '<button class="page-link" data-page="'+(resultMap.totalCnt - resultMap.pageSize)+'" data-pageSize="'+resultMap.pageSize+'" onclick="fnPageMove(this)">';
+    html += '<i class="tf-icon bx bx-chevrons-right"></i>';
+    html += '</button>';
+    html += '</li>';
+  }
+
+  document.getElementById('pagination').innerHTML = html;
+}
+
 document.addEventListener('DOMContentLoaded', function() {
   // [lnb] Current page add class active
   const lnbMenu = document.querySelectorAll('.menu-item');
@@ -12,5 +63,11 @@ document.addEventListener('DOMContentLoaded', function() {
       element.classList.remove('active');
     }
   });
+
+  if (location.pathname.indexOf('list') > -1) {
+    document.getElementById('search').addEventListener('keyup', function(event) {
+      if (event.key === "Enter") fnSearch();
+    });
+  }
 });
 
