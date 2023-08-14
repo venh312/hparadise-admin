e9cfd69f29505bb779e2378c69f2dc48041ddd3d
venh <conf312@naver.com>
2023-08-14 19:55:18 +0900

    [↔] contextLoads 제거

--- src/test/java/com/hparadise/admin/AdminApplicationTests.java
+++ src/test/java/com/hparadise/admin/AdminApplicationTests.java
@@ -1,13 +1,7 @@
 package com.hparadise.admin;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class AdminApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
 }
