418a69976152a09ff960f82632cfc3455d24f916
conf312 <conf312@naver.com>
2023-08-17 20:24:31 +0900

    Feature/work window (#32)
    * [+] 페이징 동적 처리
    
    * [+] 시작, 종료일시 조건 추가
    
    * [+] containsTarget 조건추가, readOnly 옵션 추가
    
    * [+] AccessLevel 추가
    
    * [+] 공통 함수로 이동, 날짜 변경 이벤트 함수 추가
    
    * [+] toStringDateTime 공통 유틸 클래스로 이동
    
    * [+] toStringDateTime 공통 유틸 클래스로 이동
    
    * [+] 타겟 URL 변경
    
    * [+] 역할 selected 추가
    
    * [+] 동적 링크로 변경
    
    * [+] 고객 문의 화면, MVC 가이드 작성
    
    * [+] 고객 문의 답변 등록
    
    * [↔] 쿼리스트링 수정
    
    * [↔] 미구현 페이지 alert 처리
    
    * [↔] contextLoads 제거
    
    * [↔] 프로그램 메뉴 생성
    
    * [↔] 프로그램 상세 가이드 작성
    
    * [↔] 가운데 정렬 추가
    
    * [↔] 가운데 정렬 추가
    
    * [↔] 프로그램 등록

---
+++ src/main/java/com/hparadise/admin/dto/program/ProgramSaveRequest.java
@@ -0,0 +1,34 @@
+package com.hparadise.admin.dto.program;
+
+import com.hparadise.admin.domain.program.Program;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+
+@Getter
+public class ProgramSaveRequest {
+    private LocalDateTime startDate;
+    private LocalDateTime endDate;
+    private String title;
+    private String contents;
+    private String place;
+    private String status;
+    private String useYn;
+    private Long teacherId;
+    private Long createdId;
+
+    public Program toEntity() {
+        return Program.builder()
+            .startDate(getStartDate())
+            .endDate(getEndDate())
+            .title(getTitle())
+            .contents(getContents())
+            .place(getPlace())
+            .status(getStatus())
+            .useYn(getUseYn())
+            .teacherId(getTeacherId())
+            .createdId(getCreatedId())
+            .build();
+    }
+
+}
