6371410d285f5a49125ff1d0e94d87dee5d12b63
VenhMac <gpfm312@gmail.com>
2023-08-20 16:46:48 +0900

    [+] 관리자 생년월일 컬럼 추가

--- src/main/java/com/hparadise/admin/dto/member/MemberSaveRequest.java
+++ src/main/java/com/hparadise/admin/dto/member/MemberSaveRequest.java
@@ -7,14 +7,16 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Getter
 public class MemberSaveRequest {
-    private String name;
     private String email;
+    private String name;
+    private String birth;
     private String password;
 
     public Member toEntity() {
         return Member.builder()
-            .name(getName())
             .email(getEmail())
+            .name(getName())
+            .birth(getBirth())
             .password(new BCryptPasswordEncoder().encode(getPassword()))
             .tempPasswordYn("N")
             .useYn("Y")
